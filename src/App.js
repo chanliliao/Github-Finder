43850c6222133be3e82851261b4e3032fb291e63
Chan Li Liao <cliao119@gmail.com>
2020-12-02 22:45:24 -0500

    started the UI for users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
 import About from './components/pages/About';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -38,6 +41,18 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // get single user
+  getUser = async (username) => {
+    this.setState({
+      loading: true,
+    });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = () => {
     this.setState({
       users: [],
@@ -54,7 +69,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
       <Router>
@@ -80,6 +95,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
