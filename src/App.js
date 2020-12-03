34c12df2b45b2ddd99fdfc59fa814ea70eda981e
Chan Li Liao <cliao119@gmail.com>
2020-12-02 21:57:17 -0500

    finished alert comp

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layouts/Alert';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -41,6 +43,14 @@ class App extends Component {
     });
   };
 
+  setAlert = (msg, type) => {
+    this.setState({
+      alert: { msg, type },
+    });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -49,10 +59,12 @@ class App extends Component {
         <Navbar title='Github Finder' />
 
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
